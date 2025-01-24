b79c368918f00b06e0c94d7092941662b020b6be
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2025-01-24 14:18:56 -0600

    Added vintagestoryjoin redirection

---
+++ vintagestoryjoin/scripts/main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	var current = window.location.href;
+	var url = new URL(current);
+	var ipPort = url.searchParams.get("ip") + url.searchParams.get("port");
+	window.location.href = "vintagestoryjoin://" + ipPort;
+};
