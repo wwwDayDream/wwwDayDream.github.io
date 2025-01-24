e3577e5c6f9ab3e137d4fdac2502f6aa5ac48329
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2025-01-24 14:53:49 -0600

    Trying href instead of redirect

--- vintagestoryjoin/scripts/main.js
+++ vintagestoryjoin/scripts/main.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	var current = window.location.href;
 	var url = new URL(current);
 	var ipPort = url.searchParams.get("ip") + ":" + url.searchParams.get("port");
-	window.location.replace("vintagestoryjoin://" + ipPort);
+	window.location.href = "vintagestoryjoin://" + ipPort;
 	setTimeout(() => {
 		window.close();
 	}, 100);
