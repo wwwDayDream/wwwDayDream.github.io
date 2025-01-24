885e8a7735f93bbaa37803d1ae0a813f7389c592
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2025-01-24 14:47:24 -0600

    Switching to location replace

--- vintagestoryjoin/scripts/main.js
+++ vintagestoryjoin/scripts/main.js
@@ -2,8 +2,5 @@ window.onload = function() {
 	var current = window.location.href;
 	var url = new URL(current);
 	var ipPort = url.searchParams.get("ip") + ":" + url.searchParams.get("port");
-	windowProxy = window.open("vintagestoryjoin://" + ipPort);
-	windowProxy.addEventListener("unload", ev => {
-		window.close();
-	})
+	window.location.replace("vintagestoryjoin://" + ipPort);
 };
