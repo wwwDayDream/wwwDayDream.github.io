ab47d42c832a6300da57b00cc6cf03d7f86a59f2
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2025-01-26 23:21:24 -0600

    Updated everything lol

---
+++ vintagestoryjoin/scripts/main.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+	var current = window.location.href;
+	var url = new URL(current);
+	var ip = url.searchParams.get("ip");
+	var port = url.searchParams.get("port");
+	var ipPort = ip + ":" + port;
+
+	if (!ip || !port) {
+		document.getElementById("hidden-form").style.visibility = "visible"
+		return;
+	}
+	window.location.href = "vintagestoryjoin://" + ipPort;
+};
+function generateAndCopyLink() {
+	const ip = document.getElementById('ip').value;
+	const port = document.getElementById('port').value;
+
+	if (!ip || !port) {
+		alert('Please enter both IP and Port.');
+		return;
+	}
+
+	const link = `https://wwwdaydream.github.io/vintagestoryjoin?ip=${ip}&port=${port}`;
+	navigator.clipboard.writeText(link).then(() => {
+	}).catch(err => {
+		console.error('Failed to copy:', err);
+	});
+}
